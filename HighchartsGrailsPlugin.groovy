0f82f4a40267c8b4883636898c7f74170128f8a9
Nick Maxwell <nickmaxwell10@gmail.com>
2012-11-19 13:20:17 -0500

    updated to Highcharts-2.3.3

--- HighchartsGrailsPlugin.groovy
+++ HighchartsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class HighchartsGrailsPlugin {
     // the plugin version
-    def version = "2.2.3"
+    def version = "2.3.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
